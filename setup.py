6b37a5bf9b4adfdb302c46d85bc457460dc747fa
Sofiane Soussi <sofiane.soussi@gmail.com>
2022-01-16 00:46:24 +0100

    Minor changes in setup.py

--- setup.py
+++ setup.py
@@ -4,50 +4,50 @@
 
 from setuptools import setup
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', "terminaltables==3.1.*",]
+requirements = ["Click>=7.0", "terminaltables==3.1.*", ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner", ]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ["pytest>=3", ]
 
 setup(
     author="Sofiane Soussi",
-    author_email='sofiane.soussi@gmail.com',
-    python_requires='>=3.5',
+    author_email="sofiane.soussi@gmail.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Mortgage simulator based on Swedish bank rules",
     entry_points={
-        'console_scripts': [
-            'mortgage_simulator=mortgage_simulator.simulate_mortgage:loan_simulation',
+        "console_scripts": [
+            "mortgage-simulator=mortgage_simulator.simulate_mortgage:loan_simulation",
         ],
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='mortgage_simulator',
-    name='mortgage_simulator',
+    keywords="mortgage_simulator",
+    name="mortgage-simulator",
     packages=["mortgage_simulator"],
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/sofglide/mortgage-simulator',
-    version='0.1.0',
+    url="https://github.com/sofglide/mortgage-simulator",
+    version="0.1.0",
     zip_safe=False,
 )
